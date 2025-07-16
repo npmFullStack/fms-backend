52a0ac76364e02e4f10f48cc2254469201de1c66
npmFullStack <occ.mangorangca.norway@gmail.com>
2025-07-16 21:29:54 +0800

    lol

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,15 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { loginUser, registerUser } = require('../controllers/authController');
 
-
-router.post('/register', (req, res) => {
-  console.log('Register route reached');
-  registerUser(req, res);
-});
-
-router.post('/login', (req, res) => {
-  console.log('Login route reached');
-  loginUser(req, res);
-});
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
