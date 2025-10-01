dbdd8bc945a1c6f723e0f92234f61cc9c8b4fc78
npmFullStack <occ.mangorangca.norway@gmail.com>
2025-10-02 00:36:39 +0800

    added

--- models/TruckingCompany.js
+++ models/TruckingCompany.js
@@ -2,96 +2,40 @@ import { pool } from "../db/index.js";
 
 export const getAllTruckingCompanies = async () => {
   return await pool.query(
-    `SELECT tc.*, tcd.logo_url 
-     FROM trucking_companies tc
-     LEFT JOIN trucking_company_details tcd ON tc.id = tcd.trucking_company_id
-     ORDER BY tc.name`
+    `SELECT * FROM trucking_companies
+     ORDER BY name`
   );
 };
 
 export const getTruckingCompanyById = async (id) => {
   return await pool.query(
-    `SELECT tc.*, tcd.logo_url 
-     FROM trucking_companies tc
-     LEFT JOIN trucking_company_details tcd ON tc.id = tcd.trucking_company_id
-     WHERE tc.id = $1`,
+    `SELECT * FROM trucking_companies WHERE id = $1`,
     [id]
   );
 };
 
 export const createTruckingCompany = async (id, name, logoUrl) => {
-  const client = await pool.connect();
-  try {
-    await client.query('BEGIN');
-    
-    // Insert into trucking_companies
-    await client.query(
-      'INSERT INTO trucking_companies (id, name) VALUES ($1, $2)',
-      [id, name]
-    );
-    
-    // Insert into trucking_company_details if logo exists
-    if (logoUrl) {
-      await client.query(
-        'INSERT INTO trucking_company_details (trucking_company_id, logo_url) VALUES ($1, $2)',
-        [id, logoUrl]
-      );
-    }
-    
-    await client.query('COMMIT');
-  } catch (error) {
-    await client.query('ROLLBACK');
-    throw error;
-  } finally {
-    client.release();
-  }
+  return await pool.query(
+    `INSERT INTO trucking_companies (id, name, logo_url)
+     VALUES ($1, $2, $3)`,
+    [id, name, logoUrl || null]
+  );
 };
 
 export const updateTruckingCompany = async (id, name, logoUrl) => {
-  const client = await pool.connect();
-  try {
-    await client.query('BEGIN');
-    
-    // Update trucking_companies
-    await client.query(
-      'UPDATE trucking_companies SET name = $1 WHERE id = $2',
-      [name, id]
-    );
-    
-    // Update or insert trucking_company_details
-    const detailsExist = await client.query(
-      'SELECT 1 FROM trucking_company_details WHERE trucking_company_id = $1',
-      [id]
-    );
-    
-    if (logoUrl) {
-      if (detailsExist.rows.length) {
-        await client.query(
-          'UPDATE trucking_company_details SET logo_url = $1 WHERE trucking_company_id = $2',
-          [logoUrl, id]
-        );
-      } else {
-        await client.query(
-          'INSERT INTO trucking_company_details (trucking_company_id, logo_url) VALUES ($1, $2)',
-          [id, logoUrl]
-        );
-      }
-    } else if (detailsExist.rows.length) {
-      await client.query(
-        'UPDATE trucking_company_details SET logo_url = NULL WHERE trucking_company_id = $1',
-        [id]
-      );
-    }
-    
-    await client.query('COMMIT');
-  } catch (error) {
-    await client.query('ROLLBACK');
-    throw error;
-  } finally {
-    client.release();
-  }
+  return await pool.query(
+    `UPDATE trucking_companies
+     SET name = $1,
+         logo_url = $2,
+         updated_at = NOW()
+     WHERE id = $3`,
+    [name, logoUrl || null, id]
+  );
 };
 
 export const deleteTruckingCompanyById = async (id) => {
-  return await pool.query("DELETE FROM trucking_companies WHERE id = $1", [id]);
+  return await pool.query(
+    `DELETE FROM trucking_companies WHERE id = $1`,
+    [id]
+  );
 };
